8d3aad0cc44a153aba364b77ecdf1119df239d23
David Longworth <dave@abovebored.co.uk>
2018-02-22 10:31:58 +0000

    article wip

--- web/workspace/utils/helpers/markdown.js
+++ web/workspace/utils/helpers/markdown.js
@@ -1,5 +1,5 @@
-var dust = require('dustjs-linkedin')
-var marked = require('marked')
+const dust = require('dustjs-linkedin')
+const marked = require('marked')
 
 /*
 * More info: https://github.com/chjj/marked/blob/master/README.md
@@ -11,7 +11,7 @@ marked.setOptions({
   smartLists: false,
   smartypants: true,
   sanitize: false
-});
+})
 
 /*
 * Returns the markdown content formatted as HTML
@@ -20,10 +20,10 @@ dust.helpers.markdown = function(chunk, context, bodies, params) {
   if (bodies.block) {
     return chunk.capture(bodies.block, context, function(string, chunk) {
       chunk.end(marked(string))
-    });
+    })
   }
-  return chunk;
-};
+  return chunk
+}
 
 /*
 * Returns the markdown content formatted as HTML, without wrapping p tags
@@ -31,16 +31,16 @@ dust.helpers.markdown = function(chunk, context, bodies, params) {
 dust.helpers.soberMarkdown = function(chunk, context, bodies, params) {
   if (bodies.block) {
     return chunk.capture(bodies.block, context, function(string, chunk) {
-      var md = marked(string);
+      var md = marked(string)
 
       // Replace </p><p> with <br>
-      var str = md.replace(/<\/p><p[^>]*>/igm, '<br>');
+      var str = md.replace(/<\/p><p[^>]*>/igm, '<br>')
 
       // Remove wrapping <p></p> tags
-      str = str.replace(/<p[^>]*>(.*?)<\/p>/igm, "$1");
+      str = str.replace(/<p[^>]*>(.*?)<\/p>/igm, "$1")
 
-      chunk.end(str);
-    });
+      chunk.end(str)
+    })
   }
-  return chunk;
-};
+  return chunk
+}
