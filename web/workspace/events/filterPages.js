3ac9f8288029bf19a93f80a965adaac2e8637045
David Longworth <dave@abovebored.co.uk>
2018-02-23 16:03:15 +0000

    pages filterEvent

---
+++ web/workspace/events/filterPages.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const url = require('url')
+
+const Event = function (req, res, data, callback) {
+  const pathname = url.parse(req.url).pathname
+  const parts = url.parse(req.url).pathname.split('/')
+  const slug = parts[2]
+
+  const filter = { slug: slug }
+  callback(null, filter)
+}
+
+module.exports = function (req, res, data, callback) {
+  return new Event(req, res, data, callback)
+}
+
+module.exports.Event = Event
