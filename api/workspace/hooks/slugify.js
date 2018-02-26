c394b5149cae559c1b92a810ed8ea5f1a3bc02b8
David Longworth <dave@abovebored.co.uk>
2018-02-26 18:42:59 +0000

    lowercase slugify

--- api/workspace/hooks/slugify.js
+++ api/workspace/hooks/slugify.js
@@ -2,7 +2,7 @@ const slugify = require('slugify')
 
 module.exports = function (obj, type, data) {
   if (obj[data.options.from]) {
-    obj[data.options.to] = slugify(obj[data.options.from])
+    obj[data.options.to] = slugify(obj[data.options.from], { lower: true })
   }
   return obj
 }
--- web/workspace/utils/helpers/slugify.js
+++ web/workspace/utils/helpers/slugify.js
@@ -4,7 +4,7 @@ const slugify = require('slugify')
 dust.helpers.slugify = function(chunk, context, bodies, params) {
   if (bodies.block) {
     return chunk.capture(bodies.block, context, function(string, chunk) {
-      chunk.end(slugify(string))
+      chunk.end(slugify(string, { lower: true }))
     })
   }
   return chunk
