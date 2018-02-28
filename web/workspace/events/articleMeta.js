f20a1680dedb7c436c77444fd523ade5eb2bf697
David Longworth <dave@abovebored.co.uk>
2018-02-28 20:59:06 +0000

    add images into read time

--- web/workspace/events/articleMeta.js
+++ web/workspace/events/articleMeta.js
@@ -1,6 +1,9 @@
 const Event = function (req, res, data, callback) {
+  // Get article body
+  const text = data.articles.results[0].body
+
   //split text by spaces to define total words
-  const totalWords = data.articles.results[0].body.trim().split(/\s+/g).length
+  const totalWords = text.trim().split(/\s+/g).length
 
   //define words per second based on words per minute
   const wordsPerSecond = 275 / 60
@@ -8,8 +11,16 @@ const Event = function (req, res, data, callback) {
   //define total reading time in seconds
   const totalReadingTimeSeconds = totalWords / wordsPerSecond
 
+  // Add images
+  const images = text.match(/!\[/gmi)
+  const imagesCount = images ? images.length : 0  
+  const imageSeconds = imagesCount * 12
+
+  console.log('####################################')
+  console.log(imageSeconds)
+
   // define reading time
-  data.readingTime = Math.floor(totalReadingTimeSeconds / 60)
+  data.readingTime = Math.round((totalReadingTimeSeconds + imageSeconds) / 60)
 
   callback()
 }
