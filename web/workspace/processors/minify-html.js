d57da744ad998dd53ce8f3b6072f00ca1aa91e2d
David Longworth <dave@abovebored.co.uk>
2018-03-09 19:16:42 +0000

    tweaks

--- web/workspace/processors/minify-html.js
+++ web/workspace/processors/minify-html.js
@@ -2,11 +2,13 @@ const minify = require('html-minifier').minify
 
 module.exports = (data, output) => {
   return minify(output, {
+  	collapseBooleanAttributes: true,
+  	collapseInlineTagWhitespace: true,
     collapseWhitespace: true,
     minifyCSS: true,
     minifyJS: true,
     removeRedundantAttributes: true,
     useShortDoctype: true,
-    removeComments: true
+    removeComments: false
   })
 }
