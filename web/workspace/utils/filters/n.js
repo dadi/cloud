dc12d6e0314c8b0eb5eda8ea5f64863d2586d9e3
David Longworth <david@longworth.to>
2018-04-10 13:21:09 +0100

    feat: add tweets to homepage

--- web/workspace/utils/filters/n.js
+++ web/workspace/utils/filters/n.js
@@ -2,6 +2,6 @@ const dust = require('dustjs-linkedin')
 
 // Localises a number e.g, 1000000 becomes 1,000,000
 
-dust.filters.n = function(value) {
-  return value.toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 4});
+dust.filters.n = value => {
+  return value.toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 4})
 }
