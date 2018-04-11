804f8f59e488c6de74cf6d5fb0dd522ce4e1a501
David Longworth <david@longworth.to>
2018-04-11 12:50:31 +0100

    feat: add tweets

---
+++ web/workspace/utils/helpers/tweet.js
@@ -0,0 +1,41 @@
+const dust = require('dustjs-linkedin')
+
+const username = /@(\w+)/gmi
+const links = /(https?:\/\/[^\s]+)/gmi
+const hashtags = /(^|\s)#([\w/]*)/gmi
+const newlines = /(\r\n|\r|\n)/gmi
+const tco = /(https?:\/\/t.co[^\s]+)/gmi
+
+const url = 'https://twitter.com'
+
+dust.helpers.tweet = function(chunk, context, bodies, params) {
+  if (bodies.block) {
+    return chunk.capture(bodies.block, context, function(string, chunk) {
+      // More basic url handling
+      // string = string.replace(links, `<a href="$&" target="_blank">$&</a>`)
+
+      // Resolve urls
+      if (params.urls) {
+        params.urls.forEach(i => {
+          let find = new RegExp(i.url, 'gm')
+          string = string.replace(find, `<a href="${i.expanded_url}" target="_blank">${i.display_url}</a>`)
+        })
+      }
+
+      // Remove remaining t.co urls
+      string = string.replace(tco, '')
+
+      // Hashtags
+      string = string.replace(hashtags, `<a href="${url}/hashtag/$2" target="_blank">#$2</a>`)
+
+      // Usernames
+      string = string.replace(username, `<a href="${url}/$1" target="_blank">$&</a>`)
+
+      // Line breaks
+      string = string.replace(newlines, `<br>`)
+
+      chunk.end(string)
+    })
+  }
+  return chunk
+}
