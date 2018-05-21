a3304daa1db11ee3856272c49fd8067ff75eb116
David Longworth <david@longworth.to>
2018-05-21 18:05:22 +0100

    fix: event only fires if article DS present

--- web/workspace/events/articleReadingTime.js
+++ web/workspace/events/articleReadingTime.js
@@ -1,25 +1,27 @@
 const Event = function (req, res, data, callback) {
   // Get article body
-  const text = data.articles.results[0].body || ''
+  if (data.hasResults('articles')) {
+    const text = data.articles.results[0].body || ''
 
-  // Split text by spaces to define total words
-  const totalWords = text.trim().split(/\s+/g).length
+    // Split text by spaces to define total words
+    const totalWords = text.trim().split(/\s+/g).length
 
-  // Define words per second based on words per minute
-  const wordsPerSecond = 275 / 60
+    // Define words per second based on words per minute
+    const wordsPerSecond = 275 / 60
 
-  // Define total reading time in seconds
-  const readingSeconds = totalWords / wordsPerSecond
+    // Define total reading time in seconds
+    const readingSeconds = totalWords / wordsPerSecond
 
-  // Add images
-  const images = text.match(/!\[/gmi)
-  const imagesCount = images ? images.length : 0  
-  const imageSeconds = imagesCount * 12
+    // Add images
+    const images = text.match(/!\[/gmi)
+    const imagesCount = images ? images.length : 0  
+    const imageSeconds = imagesCount * 12
 
-  // define reading time
-  const readingTime = Math.round((readingSeconds + imageSeconds) / 60)
+    // define reading time
+    const readingTime = Math.round((readingSeconds + imageSeconds) / 60)
 
-  data.readingTime = readingTime === 0 ? '<1' : readingTime
+    data.readingTime = readingTime === 0 ? '<1' : readingTime
+  }
 
   callback()
 }
