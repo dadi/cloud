544964c79d0d8521d5bc7179919667217c498e8f
David Longworth <david@longworth.to>
2018-09-05 17:12:22 +0100

    fix: redirect to deafult

--- web/workspace/events/i18n.js
+++ web/workspace/events/i18n.js
@@ -6,26 +6,29 @@ const bypassUrls = []
 
 // Populate these with a datasource
 let supportedLangs = []
-let primaryLang = ''
+let primaryLang = 'en'
+let currentLang = 'English'
 
 const Event = function (req, res, data, callback) {
-  // Get languages collection
-  supportedLangs = data.languages.results.map(lang => lang.code)
-  primaryLang = data.languages.results.filter(lang => lang.primary)[0].code
-  currentLang = data.languages.results.filter(lang => lang.code === data.params.lang)[0].name
-
   // Path without trailing slash & default lang
   let toPath = url.parse(req.url, true).path.replace(/\/+$/, '')
   let firstFolder = toPath.replace(/^\/([^\/]*).*$/, '$1')
 
-  // Remove firstFolder
-  if (supportedLangs.includes(firstFolder)) {
-    var rmvLang = new RegExp('^\/' + firstFolder, 'gmi')
-    toPath = toPath.replace(rmvLang, '')
-  }
-
   // Add it to the page data
   data.pathNoLang = toPath
+
+  // Get languages collection
+  if (data.hasResults('languages') && data.params.lang) {
+    supportedLangs = data.languages.results.map(lang => lang.code)
+    primaryLang = data.languages.results.filter(lang => lang.primary)[0].code
+    currentLang = data.languages.results.filter(lang => lang.code === data.params.lang)[0].name
+  }
+
+  // Remove firstFolder
+  if (supportedLangs.includes(firstFolder)) {
+    let rmvLang = new RegExp('^\/' + firstFolder, 'gmi')
+    toPath = toPath.replace(rmvLang, '')
+  }
 
   // Do nothing if default language
   if (!(supportedLangs.includes(firstFolder)) && !(bypassUrls.includes(toPath))) { 
