907a962c1f859f6d9e4cf8c260bc50797473e09a
David Longworth <david@longworth.to>
2018-09-05 17:01:33 +0100

    feat: lang dropdown

--- web/workspace/events/i18n.js
+++ web/workspace/events/i18n.js
@@ -1,34 +1,43 @@
 const url = require('url')
 const path = require('path')
 
-// Set the primary language
-const primaryLang = 'en'
-
-// Set what we support
-const supportedLangs = [primaryLang, 'pt']
+// Urls in which this event will not fire
 const bypassUrls = []
 
+// Populate these with a datasource
+let supportedLangs = []
+let primaryLang = ''
+
 const Event = function (req, res, data, callback) {
+  // Get languages collection
+  supportedLangs = data.languages.results.map(lang => lang.code)
+  primaryLang = data.languages.results.filter(lang => lang.primary)[0].code
+  currentLang = data.languages.results.filter(lang => lang.code === data.params.lang)[0].name
+
   // Path without trailing slash & default lang
   let toPath = url.parse(req.url, true).path.replace(/\/+$/, '')
-  const firstFolder = toPath.replace(/^\/([^\/]*).*$/, '$1')
+  let firstFolder = toPath.replace(/^\/([^\/]*).*$/, '$1')
 
   // Remove firstFolder
-  if (~(supportedLangs.indexOf(firstFolder))) {
+  if (supportedLangs.includes(firstFolder)) {
     var rmvLang = new RegExp('^\/' + firstFolder, 'gmi')
     toPath = toPath.replace(rmvLang, '')
   }
 
+  // Add it to the page data
   data.pathNoLang = toPath
 
   // Do nothing if default language
-  if (!~(supportedLangs.indexOf(firstFolder)) && !~(bypassUrls.indexOf(toPath))) { 
+  if (!(supportedLangs.includes(firstFolder)) && !(bypassUrls.includes(toPath))) { 
     res.writeHead(302, { Location: '/' + primaryLang + toPath })
     return res.end()
   }
 
+  // Add it to the page data
   data.lang = data.params.lang || primaryLang
-  
+  data.currentLang = currentLang
+    
+  // Fin
   callback()
 }
 
