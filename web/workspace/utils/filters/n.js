7f319e62390388773bba19c12c8f339bf3288dad
James Lambie <jameslambie@gmail.com>
2018-11-05 10:20:13 +0100

    fix: add rounding to USD amounts on token page

--- web/workspace/utils/filters/n.js
+++ web/workspace/utils/filters/n.js
@@ -3,5 +3,10 @@ const dust = require('dustjs-linkedin')
 // Localises a number e.g, 1000000 becomes 1,000,000
 
 dust.filters.n = value => {
-  return value.toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 4})
+  return value.toLocaleString(undefined,
+    {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: (value > 10000) ? 2 : 4
+    }
+  )
 }
